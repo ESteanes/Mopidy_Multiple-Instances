1e4effb4df377ab8dcb7044fbcacb4070f3cf1ef
Ellington Steanes <skellsrsmail@gmail.com>
2021-08-01 18:38:14 +1000

    Won't create redundant lines in config file

--- multiple_mopidy.sh
+++ multiple_mopidy.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+if  test $2 -eq "" 
+then
+    hostname="127.0.0.1"
+else
+    hostname=$2
+fi 
+
+coreconfig="/etc/mopidy/mopidy.conf"
 i=1
 while [ $i -le $(($1)) ]; do
 echo "Making additional Mopidy instance: $1"
@@ -17,13 +25,24 @@ output = audioresample ! audioconvert ! audio/x-raw,rate=48000,channels=2,format
 
 echo "Executing Step 4"
 host=`sudo cat /etc/mopidy.conf | grep "^hostname" | sed "s/^[a-zA-Z =]*//g"`
-echo "host = $host"
-sudo sed -i "/^allowed_origins/ s/$/,$host:668$i/" /etc/mopidy.conf
+if test $host -ne ""
+then
+    hostname=$host
+fi
+echo "hostname = $hostname"
 
+# latest mopidy.conf file doesn't have much content in it.
+if grep -q "^allowed_origins" /etc/mopidy/mopdiy.conf
+then
+    #need to check that we won't write a duplicate host name
+    sudo sed -i "/^allowed_origins/ s/$/,$hostname:668$i/" $coreconfig
+else
+    echo "allowed_origins = $hostname:6680,$hostname:668$i" >> $coreconfig
+fi
 
 echo "Executing Step 5"
-sudo mkdir /var/cache/mopdiy_$i
-sudo mkdir /var/lib/mopidy_$i
+sudo mkdir -p /var/cache/mopdiy_$i
+sudo mkdir -p /var/lib/mopidy_$i
 sudo chown mopidy:audio /var/cache/mopidy_$i /var/lib/mopidy_$i
 
 echo "Executing Step 6"
@@ -35,7 +54,11 @@ sudo cp /lib/systemd/system/mopidy.service /lib/systemd/system/mopidy_$i.service
 sed -i "s/mopidy.conf$/mopidy.conf\:\/etc\/mopidy\/mopidy_1.conf\"/g" /lib/systemd/system/mopidy_$i.service
 
 echo "Executing Step 8"
-sudo sed -i "/\[stream\]/a pipe:\/\/\/tmp\/snapfifo_$i?name=Stream_$i" /etc/snapserver.conf
+#need to check we aren't duplicating stream entries
+if test `sudo grep "pipe:\/\/\/tmp\/snapfifo_$i" /etc/snapserver.conf | wc -l` -eq 0
+then
+    sudo sed -i "/\[stream\]/a source = pipe:\/\/\/tmp\/snapfifo_$i?name=Stream_$i" /etc/snapserver.conf
+fi
 
 echo "Executing Step 9"
 sudo "mopidy_$1" local scan
